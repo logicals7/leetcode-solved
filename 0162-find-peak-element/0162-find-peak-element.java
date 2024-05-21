927148f735859f7dafe5f60a7b5239acedd07e79
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-21 12:03:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (26.22%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,3 +1,25 @@
+//Using binary search
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        else if(nums[n-1] > nums[n-2]) return n-1;
+
+        int low = 1, high = n-2;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
+            else if(nums[mid] > nums[mid-1]) low = mid+1;
+            else high = mid-1;
+        }  
+        return -1;      
+    }
+}
+
+
+/*
+//using linear search
 class Solution {
     public int findPeakElement(int[] arr) {
         int n = arr.length;
@@ -22,3 +44,5 @@ class Solution {
         
     }
 }
+
+*/
