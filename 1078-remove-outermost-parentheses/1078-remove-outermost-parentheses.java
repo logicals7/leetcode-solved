7afc118b435373bb3efd39d91ba3c72db2d38cce
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-21 22:19:32 +0530

    Time: 10 ms (21.91%), Space: 45.1 MB (15.21%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,3 +1,31 @@
+//using stack
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        String ans = "";
+        int start = 0; // to mark the start of a primitive string
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') st.push(s.charAt(i));
+            else st.pop();
+
+            //if stack is empty, then we have one complete segment
+            //remove its outermost parentheses & store in result
+            if(st.isEmpty()){
+                ans += s.substring(start+1, i);
+                start = i+1;
+            }
+        }
+        return ans;
+        
+    }
+}
+
+
+
+
+/*
+//using brute force
 class Solution {
     public String removeOuterParentheses(String s) {
         int count = 0;
@@ -6,13 +34,13 @@ class Solution {
         //          ( ( ) ( ) ) ( ( ) )
         //count =   1 2 1 2 1 0 1 2 1 0
         //when count is 1, there is new segment, means new outer bracket
-        //
+        //sequence of conditions written below in necessary
 
         for(int i = 0; i < s.length(); i++){
             if((count != 1 && s.charAt(i) != '(') || count != 0 && s.charAt(i) != ')'){
                 ans += s.charAt(i);
             }
-            
+
             if(s.charAt(i) == '('){
                 count++;
             }
@@ -25,3 +53,5 @@ class Solution {
         
     }
 }
+
+*/
