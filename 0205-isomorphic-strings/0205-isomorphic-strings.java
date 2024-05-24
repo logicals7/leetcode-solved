2eae5757ce80078c072f78db1242de02b6c71ec9
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-25 01:32:23 +0530

    Time: 16 ms (24.39%), Space: 42.3 MB (61.37%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+
         if (s.length() != t.length()) {
             return false;
         }
 
+
         HashMap<Character, Character> sTot = new HashMap<>();
         HashMap<Character, Character> tTos = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
+
             char a = s.charAt(i);
             char b = t.charAt(i);
 
-            if (sTot.containsKey(a)) {
-                if (sTot.get(a) != b) {
-                    return false;
-                }
-            } else {
-                sTot.put(a, b);
+            //if the Hashmap stoT contains the key already
+            if(sTot.containsKey(a)){
+                //then it should match the current char in t string
+                if(sTot.get(a) != b) return false;
             }
+            //if the Hashmap sTot does not contain the key
+            else sTot.put(a, b);
 
-            if (tTos.containsKey(b)) {
-                if (tTos.get(b) != a) {
-                    return false;
-                }
-            } else {
-                tTos.put(b, a);
+            //if the HashMap tTos contains the key already
+            if(tTos.containsKey(b)){
+                //then it should match the current char in s string
+                if(tTos.get(b) != a) return false;
             }
+            //if the HashMap tTos does not contain the key
+            else tTos.put(b, a);
         }
-        
-        return true;
+        return true;        
     }
 }
