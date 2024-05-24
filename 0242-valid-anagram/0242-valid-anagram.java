63a760709ec55575a70a7fdcfe9cd386a6ebc6d4
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-25 02:02:32 +0530

    Time: 6 ms (44.43%), Space: 42.7 MB (87.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            // char c1 = s.charAt(i); // Get character from s at index i
+            // int index1 = c1 - 'a'; // Calculate index in charCount array
+            // charCount[index1]++;    // Increment count at calculated index
+            //or we could write above lines as:
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0 ; i < arr.length; i++)
+            if(arr[i] != 0) return false;
+
+        return true;
+        
+    }
+}
+
+
+
+/*
+//method-1: using nlogn sorting
+class Solution {
+    public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
@@ -14,3 +39,4 @@ class Solution {
         
     }
 }
+*/
