59be3c7834ace547b9c5557a99efe3ab94b850dd
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-30 05:38:14 +0530

    Time: 0 ms (100%), Space: 44.3 MB (69.78%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,6 +9,31 @@
  *     }
  * }
  */
+
+//Solution-2: using Floyd's Cycle Detection Algo/ Tortoise Hare Algo
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head, fast = head;
+        // Continue loop while there are at least two more nodes to advance 'fast' pointer
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+/*
+ //Solution-1: Using HashMap
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         if(head == null || head.next == null) return null;
@@ -29,3 +54,5 @@ public class Solution {
         
     }
 }
+
+*/
