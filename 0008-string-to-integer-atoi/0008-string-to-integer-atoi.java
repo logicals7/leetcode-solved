3e934e3e10049a8cca8bde0ae44a26bd4c79fd03
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-05-31 21:23:46 +0530

    Time: 1 ms (100%), Space: 42.3 MB (43.26%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,5 +1,51 @@
 class Solution {
     public int myAtoi(String s) {
+        //cutting the leading spaces
+        s = s.trim();
+
+        //check if the string is empty
+        if(s.equals("")) return 0;
+
+        //storing the sign
+        int sign = 1;
+        int i = 0;
+        if(s.charAt(i) == '-'){
+            sign = -1;
+            i++;
+        }
+        else if(s.charAt(i) == '+') i++;
+
+        //if there are no digits after optional sign return 0
+        if(i == s.length() || !Character.isDigit(s.charAt(i))) return 0;
+
+        //use recursion for the string after the sign if exists valid string
+        return recursiveAtoi(s, i, sign, 0);
+    }
+
+    //below is the recursive function
+    private int recursiveAtoi(String s, int i, int sign, int res){
+        //base case: if there are no digits after optional sign return 0
+        if(i == s.length() || !Character.isDigit(s.charAt(i))) return res*sign;
+
+        //get the digit
+        int digit = s.charAt(i) - '0';
+
+        //check if adding this digit causes overflow
+        //if it makes an overflow => then check if sign is 1 the return max_val  else min_val
+        long overflowVal = (long)res * 10 + digit;
+        if(overflowVal > Integer.MAX_VALUE) return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+        //update the result if not an overflow;
+        res = res * 10 + digit;
+        //make the recursive call for next char
+        return recursiveAtoi(s, i+1, sign, res);
+    }
+}
+
+
+/*
+class Solution {
+    public int myAtoi(String s) {
         // Check if the input string is null
         if (s == null) return 0;
 
@@ -44,3 +90,4 @@ class Solution {
         return (int) (sign * ans);
     }
 }
+*/
