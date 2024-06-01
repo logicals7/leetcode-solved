3ff75d87ae1412d7a47d5dd5aaa85f7ad13cb82c
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-06-01 19:39:11 +0530

    Time: 4 ms (7.44%), Space: 51.9 MB (5.03%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,3 +1,20 @@
+//reverse a string using stack
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length; i++){
+            st.push(s[i]);
+        }
+
+        for(int i = 0; i < s.length; i++){
+            s[i] = st.pop();
+        }
+
+
+    }
+}
+
+/*
 class Solution {
     public void reverseString(char[] s) {
         int i = 0, j = s.length-1;
@@ -11,3 +28,4 @@ class Solution {
         }       
     }
 }
+*/
