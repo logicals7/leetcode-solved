af67301444ebf637d16499896f8fd5bca49eb373
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-06-09 11:50:20 +0530

    Time: 0 ms (100%), Space: 41.2 MB (82.77%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,5 +1,21 @@
 class Solution {
     public int minAddToMakeValid(String s) {
+        int countOpen = 0;
+        int countClose = 0;
+        for(char c : s.toCharArray()){
+            if(c == '(') countOpen++;
+            else if(c == ')' && countOpen > 0) countOpen--;
+            else if(c == ')') countClose++;
+        }
+        return countOpen + countClose;
+        
+    }
+}
+
+
+/*
+class Solution {
+    public int minAddToMakeValid(String s) {
         Stack<Character> st = new Stack<>();
         int countCloseBracket = 0;
         int countStackElements = 0;
@@ -26,3 +42,4 @@ class Solution {
         
     }
 }
+*/
