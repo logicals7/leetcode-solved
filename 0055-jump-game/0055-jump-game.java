feb8d6d7711f0c03cea73bcf411aaaeb505ddaaf
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2024-06-19 02:45:22 +0530

    Time: 1 ms (99.97%), Space: 46.1 MB (7.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,3 +1,20 @@
+//approach: we start from the end index & if we can reach the end from end-1, 
+//then our new target is to reach the end-1
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length-1;
+        for(int i = goal-1; i >= 0; i--){
+            //if i+nums[i] > greater than goal, that means we can reach the goal from current index,
+            //so we will check that if we can reach the curr index from curr-1 index, so goal is curr index
+            if(i + nums[i] >= goal){
+                goal = i;
+            }
+        }
+        return (goal==0?true:false);
+    }
+}
+
+/*
 class Solution {
     public boolean canJump(int[] nums) {
 
@@ -19,3 +36,4 @@ class Solution {
         
     }
 }
+*/
